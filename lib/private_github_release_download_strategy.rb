a9607e01aa660e034442c2f06c0909184b38ee06
etng <547+etng@users.noreply.github.com>
2026-07-18 00:22:50 +0800

    fix(tap): 🐛 allow cached private assets in build sandbox

--- lib/private_github_release_download_strategy.rb
+++ lib/private_github_release_download_strategy.rb
@@ -6,19 +6,15 @@ require "utils/github/api"
 class PrivateGitHubReleaseDownloadStrategy < CurlDownloadStrategy
   def initialize(url, name, version, **meta)
     token = GitHub::API.credentials
-    if token.blank?
-      raise CurlDownloadStrategyError.new(
-        url,
-        "This package is stored in a private GitHub repository. Run `gh auth login` and try again.",
-      )
+    if token.present?
+      meta[:headers] ||= []
+      meta[:headers] += [
+        "Accept: application/octet-stream",
+        "Authorization: token #{token}",
+        "X-GitHub-Api-Version: 2022-11-28",
+      ]
     end
 
-    meta[:headers] ||= []
-    meta[:headers] += [
-      "Accept: application/octet-stream",
-      "Authorization: token #{token}",
-      "X-GitHub-Api-Version: 2022-11-28",
-    ]
     super
   end
 end
